1c3108b0d726d09ce78023ccb8f54a503feef04a
Takesure Mamvota <79244628+MamvotaTake@users.noreply.github.com>
2022-01-05 21:01:19 +0700

    Update default.ts

--- config/default.ts
+++ config/default.ts
@@ -4,7 +4,7 @@ export default {
     saltRounds: process.env.SALT,
     accessTokenTtl: process.env.ACCESS_TOKEN,
     refreshTokenTtl: process.env.REFRESH_TOKEN,
-    privateKey:process.env.PRIVATE_KEY,
-    publicKey:process.env.PUBLIC_KEY
+    privateKey:``,
+    publicKey:``
     PER_PAGE: 6,
 }
